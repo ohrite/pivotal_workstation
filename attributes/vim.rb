bd202eb627548cd7707c80dad01b5b02d86311f0
Doc Ritezel <pair+doc@pivotallabs.com>
2012-12-23 10:17:01 -0800

    refactor vim recipe

--- attributes/vim.rb
+++ attributes/vim.rb
@@ -1,3 +1,4 @@
 node.default["vim_home"]="#{WS_HOME}/.vim"
 node.default["vim_hash"]="c948a08572b6749b9c91aabada1f498e6e9d61a1"
 node.default["vim_config_git"]="git://github.com/pivotalcommon/vim-config.git"
+node.default["vim_homebrew_url"]="https://raw.github.com/Homebrew/homebrew-dupes/c93e84ace76c58ae2386c439040110b57e510f04/vim.rb"
