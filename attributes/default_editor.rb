5e1c1919b7c15c01564502bb4539e930f68a3440
Doc Ritezel <pair+doc@pivotallabs.com>
2012-12-23 10:15:53 -0800

    set the default editor using duti

---
+++ attributes/default_editor.rb
@@ -0,0 +1,10 @@
+# extracted with `mdls /Applications/Name.app | grep kMDItemCFBundleIdentifier`
+node.default["default_editor"]="com.sublimetext.2"
+node.default["default_editor_content_types"] = %w(
+  public.shell-script
+  public.plain-text
+  public.unix-executable
+  public.ruby-script
+  org.vim.yaml-file
+  public.xml
+)
